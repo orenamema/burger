a12cfb045dae991b605d6c932803862f17a712d1
oren amema <orenamema@gmail.com>
2020-01-20 20:02:51 -0800

    editing css

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,8 @@ USE burger_db;
 
   CREATE TABLE burgers (
   id INT NOT NULL AUTO_INCREMENT,
-  burger_name VARCHAR(30) NULL,
-  devoured BOOLEAN,
+  burger_name VARCHAR(255) NULL,
+  devoured BOOLEAN DEFAULT false,
   PRIMARY KEY (id)
   );
 
--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-var mysql = require("mysql");
 var express = require("express");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 var exphbs = require("express-handlebars");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3000,
-  user: "root",
-  password: "password",
-  database: "burger_db"
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
 });
